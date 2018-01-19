04e522bdfda907db01bddf8aa2f36b3f19e15119
Peter Indiola <pindiola@gmail.com>
2018-01-19 12:38:50 +0800

    mailjet

--- js/contact_me.js
+++ js/contact_me.js
@@ -31,18 +31,59 @@ $(function() {
         '\n\n' +
         'message : ' +
         message
-      Email.send(
-        'rupert@beconnectedtech.com.au',
-        'rupert@beconnectedtech.com.au',
-        'beConnectedTech contact form email',
-        content,
-        {token: '6e244aea-551e-482f-bab0-324459321d4d'},
-        function done(message) {
+
+      $.ajax({
+        url:
+          'https://71f86b1f6abfa58368ee279fb66f2ea5:c58f9f720497a2d3ed1bf5585facbd91@api.mailjet.com/v3.1/send',
+        type: 'POST',
+        data: {
+          subject: 'Contact Form',
+          fromAddress: 'rupert@beconnectedtech.com.au',
+          toAddress: 'rupert@beconnectedtech.com.au',
+          content: content,
+          encoding: 'UTF-8',
+        },
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>")
+          $('#success > .alert-success')
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;",
+            )
+            .append('</button>')
+          $('#success > .alert-success').append(
+            '<strong>Your message has been sent. </strong>',
+          )
           $('#success > .alert-success').append('</div>')
           //clear all fields
           $('#contactForm').trigger('reset')
         },
-      )
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>")
+          $('#success > .alert-danger')
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;",
+            )
+            .append('</button>')
+          $('#success > .alert-danger').append(
+            $('<strong>').text(
+              'Sorry ' +
+                firstName +
+                ', it seems that my mail server is not responding. Please try again later!',
+            ),
+          )
+          $('#success > .alert-danger').append('</div>')
+          //clear all fields
+          $('#contactForm').trigger('reset')
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop('disabled', false) // Re-enable submit button when AJAX call is complete
+          }, 1000)
+        },
+      })
     },
     filter: function() {
       return $(this).is(':visible')
