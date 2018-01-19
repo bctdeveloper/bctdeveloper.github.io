080b8ea6e54782d6e6712ed20c7ae510c0d3b6bd
Peter Indiola <pindiola@gmail.com>
2018-01-19 11:34:44 +0800

    updated contact form sms

--- js/contact_me.js
+++ js/contact_me.js
@@ -26,63 +26,23 @@ $(function() {
         'phone : ' +
         phone +
         '\n\n' +
+        'email : ' +
+        email +
+        '\n\n' +
         'message : ' +
         message
-
-      $.ajax({
-        url: 'https://mail.zoho.com/api/accounts/4070042000000008002/messages',
-        type: 'POST',
-        data: {
-          subject: 'Contact Form',
-          fromAddress: 'rupert@beconnectedtech.com.au',
-          toAddress: 'rupert@beconnectedtech.com.au',
-          content: content,
-          encoding: 'UTF-8',
-        },
-        headers: {
-          Authorization: 'Zoho-authtoken 06536bf03f65ed7e9fd3e9384f703cec',
-        },
-        cache: false,
-        success: function() {
-          // Success message
-          $('#success').html("<div class='alert alert-success'>")
-          $('#success > .alert-success')
-            .html(
-              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;",
-            )
-            .append('</button>')
-          $('#success > .alert-success').append(
-            '<strong>Your message has been sent. </strong>',
-          )
+      Email.send(
+        'rupert@beconnectedtech.com.au',
+        'rupert@beconnectedtech.com.au',
+        'beConnectedTech contact form email',
+        content,
+        {token: '31826667-c7f4-442c-8081-fa71343b273a'},
+        function done(message) {
           $('#success > .alert-success').append('</div>')
           //clear all fields
           $('#contactForm').trigger('reset')
         },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>")
-          $('#success > .alert-danger')
-            .html(
-              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;",
-            )
-            .append('</button>')
-          $('#success > .alert-danger').append(
-            $('<strong>').text(
-              'Sorry ' +
-                firstName +
-                ', it seems that my mail server is not responding. Please try again later!',
-            ),
-          )
-          $('#success > .alert-danger').append('</div>')
-          //clear all fields
-          $('#contactForm').trigger('reset')
-        },
-        complete: function() {
-          setTimeout(function() {
-            $this.prop('disabled', false) // Re-enable submit button when AJAX call is complete
-          }, 1000)
-        },
-      })
+      )
     },
     filter: function() {
       return $(this).is(':visible')
