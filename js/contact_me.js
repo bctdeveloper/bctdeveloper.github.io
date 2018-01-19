f19407b277bc1ee304db569f9944fa494e1ace68
Peter Indiola <pindiola@gmail.com>
2018-01-19 17:18:42 +0800

    email updates

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
         message
 
       $.ajax({
-        url: 'https://x3fa303ieh.execute-api.us-east-1.amazonaws.com/',
+        url: 'https://x3fa303ieh.execute-api.us-east-1.amazonaws.com/dev',
         type: 'POST',
         data: {
           name: name,
@@ -58,21 +58,21 @@ $(function() {
           $('#contactForm').trigger('reset')
         },
         error: function() {
-          // // Fail message
-          // $('#success').html("<div class='alert alert-danger'>")
-          // $('#success > .alert-danger')
-          //   .html(
-          //     "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;",
-          //   )
-          //   .append('</button>')
-          // $('#success > .alert-danger').append(
-          //   $('<strong>').text(
-          //     'Sorry ' +
-          //       firstName +
-          //       ', it seems that my mail server is not responding. Please try again later!',
-          //   ),
-          // )
-          // $('#success > .alert-danger').append('</div>')
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>")
+          $('#success > .alert-danger')
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;",
+            )
+            .append('</button>')
+          $('#success > .alert-danger').append(
+            $('<strong>').text(
+              'Sorry ' +
+                firstName +
+                ', it seems that my mail server is not responding. Please try again later!',
+            ),
+          )
+          $('#success > .alert-danger').append('</div>')
           //clear all fields
           $('#contactForm').trigger('reset')
         },
